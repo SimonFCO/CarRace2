5f7f099ba749044fd2b748828f247eeaad6a0344
simonFCO <simon.vare6@gmail.com>
2026-04-19 22:49:15 +0200

    Setup Getting Along

---
+++ CarRace2/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRace2
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; } = 120;
+        public bool Stopped { get; set; }
+        public Car(string name, bool stopped)
+        {
+            Name = name;
+            Stopped = stopped;
+        }
+    }
+}
---
+++ CarRace2/Events.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRace2
+{
+    internal class Events
+    {
+        
+        private static void EmptyGas(Car car)
+        {
+
+        }
+
+        private static void BlownTire(Car car)
+        {
+
+        }
+
+        private static void BirdHit(Car car)
+        {
+
+        }
+
+        private static void EngineProblem(Car car)
+        {
+
+        }
+        private static void randomEvents()
+        {
+                  
+        }
+        public static void ExecuteRandomEvent()
+        {
+
+        }
+    }
+}

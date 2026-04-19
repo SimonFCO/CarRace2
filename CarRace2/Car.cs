3fabd6d0dbeeb3c285b4599c0ba057786978d8fa
simonFCO <simon.vare6@gmail.com>
2026-04-19 23:23:35 +0200

    Car driving in individual threads completed

--- CarRace2/Car.cs
+++ CarRace2/Car.cs
@@ -10,11 +10,29 @@ namespace CarRace2
     {
         public string Name { get; set; }
         public int Speed { get; set; } = 120;
-        public bool Stopped { get; set; }
-        public Car(string name, bool stopped)
+        public bool Stopped { get; set; } = false;
+        public int PauseTimer { get; set; } = 0;
+        public int DistanceDriven { get; set; } = 0;
+        public Car(string name)
         {
             Name = name;
-            Stopped = stopped;
+        }
+
+        public void Update()
+        {
+            if(PauseTimer <= 0)
+            {
+                Stopped = false;
+            }
+            else
+            {
+                Stopped = true;
+            }
+        }
+
+        public void Drive()
+        {
+            DistanceDriven += Speed;
         }
     }
 }
--- CarRace2/Events.cs
+++ CarRace2/Events.cs
@@ -11,30 +11,57 @@ namespace CarRace2
         
         private static void EmptyGas(Car car)
         {
-
+            car.Stopped = true;
+            car.PauseTimer += 15;
         }
 
         private static void BlownTire(Car car)
         {
-
+            car.Stopped = true;
+            car.PauseTimer += 10;
         }
 
         private static void BirdHit(Car car)
         {
-
+            car.Stopped = true;
+            car.PauseTimer += 5;
         }
 
         private static void EngineProblem(Car car)
         {
-
+            car.Speed -= 1;
         }
-        private static void randomEvents()
+        private static void ExecuteRandomEvent(Car car)
         {
-                  
-        }
-        public static void ExecuteRandomEvent()
-        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 51);
 
+            switch (random)
+            {
+                case int n when (n >= 1 && n <= 2):
+                    Console.WriteLine("Slut på bensin");
+                    EmptyGas(car);
+                    break;
+
+                case int n when (n >= 3 && n <= 4): 
+                    Console.WriteLine("Punktering");
+                    BlownTire(car);
+                    break;
+
+                case int n when (n >= 5 && n <= 9): 
+                    Console.WriteLine("Fågel på vindrutan");
+                    BirdHit(car);
+                    break;
+
+                case int n when (n >= 10 && n <= 19): 
+                    Console.WriteLine("Motorfel");
+                    EngineProblem(car);
+                    break;
+
+                default:
+                    Console.WriteLine("[LOG] No issue occurred.");
+                    break;
+            }
         }
     }
 }
